858f551e18e51e586b470e27c916961f0dcfeb09
Brianna Steier <briannasteier@gmail.com>
2023-05-13 16:04:11 -0700

    Created a board and added debug print statements.

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -14,7 +14,14 @@
 
 from AI import AI
 from Action import Action
+import random
+from queue import Queue
 
+LEAVE = 0
+UNCOVER = 1
+FLAG = 2
+UNFLAG = 3
+		
 
 class MyAI( AI ):
 
@@ -23,7 +30,21 @@ class MyAI( AI ):
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
-		pass
+		self._startX = startX
+		self._startY = startY
+		self._rowDimension = rowDimension
+		self._colDimension = colDimension
+		print(startX, startY, rowDimension, colDimension, totalMines)
+		self._moveCount = 0
+
+		self._board = [[-1 for _ in range(colDimension)] for _ in range(rowDimension)]
+		self._board[(self._startY + 1)*-1][self._startX] = 0  # leave the x the same but +1 and negate for y since we start at bottom left
+		
+		self._uncovered_tiles = 0
+		self._safe_spaces = colDimension*rowDimension - totalMines
+
+		self.action_queue = Queue()
+		print(self._board)
 		########################################################################
 		#							YOUR CODE ENDS							   #
 		########################################################################
@@ -34,7 +55,29 @@ class MyAI( AI ):
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
-		return Action(AI.Action.LEAVE)
+
+		while self._moveCount != self._rowDimension*self._colDimension:
+			action = AI.Action(random.randrange(1, len(AI.Action)))
+			x = random.randrange(self._colDimension)
+			y = random.randrange(self._rowDimension)
+			self._moveCount += 1
+			return Action(action, x, y)
+
+		action = AI.Action(random.randrange(len(AI.Action)))
+		x = random.randrange(self._colDimension)
+		y = random.randrange(self._rowDimension)
+
+		print("ACTION", action, " NUMBER", number, "COORDS", x, y)
+
+		if self._uncovered_tiles == self._safe_spaces:  # we won the game
+			print("win")
+			return Action(AI.Action.LEAVE)
+		
+		return Action(action, x, y)
 		########################################################################
 		#							YOUR CODE ENDS							   #
 		########################################################################
+
+
+# run with  python3 Main.py -f .\Problems\Easy_world_1.txt for one world
+# run with  python3 Main.py -f .\Problems\ for all worlds
