8786870629d716a3c93fae9e4fbb3a53abb23110
Brianna Steier <briannasteier@gmail.com>
2023-05-13 17:43:24 -0700

    Trying to make actions work

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -20,64 +20,77 @@ from queue import Queue
 LEAVE = 0
 UNCOVER = 1
 FLAG = 2
-UNFLAG = 3
-		
+UNFLAG = 3		
 
 class MyAI( AI ):
 
 	def __init__(self, rowDimension, colDimension, totalMines, startX, startY):
-
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
+		self._coveredTile = 0 # 2 kinds marked (know there is mine) or unmakred (dont know)
 		self._startX = startX
 		self._startY = startY
 		self._rowDimension = rowDimension
 		self._colDimension = colDimension
+		self._totalMines = totalMines
 		print(startX, startY, rowDimension, colDimension, totalMines)
 		self._moveCount = 0
 
-		self._board = [[-1 for _ in range(colDimension)] for _ in range(rowDimension)]
-		self._board[(self._startY + 1)*-1][self._startX] = 0  # leave the x the same but +1 and negate for y since we start at bottom left
+		self._board = [[-1 for _ in range(colDimension)] for _ in range(rowDimension)]  #board[][]
+		self._board[rowDimension - 1 - self._startY][self._startX] = 0  # leave the x the same but +1 and negate for y since we start at bottom left
 		
 		self._uncovered_tiles = 0
 		self._safe_spaces = colDimension*rowDimension - totalMines
 
+		self._uncover = (False, (-1, -1))
 		self.action_queue = Queue()
-		print(self._board)
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+		for i in self.generate_neighbors(self._startX, self._startY):
+			self.action_queue.put(i)
 
-		
+		#print_board(self._board)
+
 	def getAction(self, number: int) -> "Action Object":
+		print_board(self._board)
+		if self._uncover[0]:
+			row = self._rowDimension- 1 - self._uncover[1][1] 
+			self._board[row][self._uncover[2][0]] = number
+			self._uncover = (False, -1, -1)
 
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-
-		while self._moveCount != self._rowDimension*self._colDimension:
-			action = AI.Action(random.randrange(1, len(AI.Action)))
-			x = random.randrange(self._colDimension)
-			y = random.randrange(self._rowDimension)
+		if (not self.action_queue.empty()):
+			# uncover tiles
+			x, y = self.action_queue.get()
+			print(x + 1, y + 1)
+			self._uncover = (True, (x, y))
 			self._moveCount += 1
-			return Action(action, x, y)
+			return Action(AI.Action(UNCOVER), 2, 2)
+	
+		#
+		# if self._uncovered_tiles == self._safe_spaces:  # we won the game
+		# 	print("win")
+		# 	return Action(AI.Action(LEAVE))
+		# return Action(AI.Action(LEAVE))
+		#
 
-		action = AI.Action(random.randrange(len(AI.Action)))
-		x = random.randrange(self._colDimension)
-		y = random.randrange(self._rowDimension)
 
-		print("ACTION", action, " NUMBER", number, "COORDS", x, y)
+	def generate_neighbors(self, x, y) -> list:
+		coords = [(x-1,y), (x-1, y-1), (x,y-1), (x+1,y-1), (x + 1,y), (x+1,y+1), (x,y+1), (x-1,y+1)]
 
-		if self._uncovered_tiles == self._safe_spaces:  # we won the game
-			print("win")
-			return Action(AI.Action.LEAVE)
-		
-		return Action(action, x, y)
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+		for c in coords[:]:
+			if c[0] < 0 or c[0] > self._colDimension - 1:
+				coords.remove(c)
+			elif c[1] < 0 or c[1] > self._rowDimension - 1:
+				coords.remove(c)
+
+		print(coords)
+		return coords
+
+	
+def print_board(board):
+	for i in board:
+		print(i)
+	print("-----------------")
 
 
 # run with  python3 Main.py -f .\Problems\Easy_world_1.txt for one world
 # run with  python3 Main.py -f .\Problems\ for all worlds
+
+# run with  python Main.py -f .\Problems\Easy_world_1.txt for one world
+# run with  python Main.py -f .\Problems\ for all worlds
