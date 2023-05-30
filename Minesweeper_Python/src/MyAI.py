03f5a187673dd296f4e624b2b22d63e6a11e445c
bsteier <briannasteier@gmail.com>
2023-05-30 15:52:32 -0700

    Started backtracking

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -53,7 +53,8 @@ class MyAI( AI ):
 		neighbors_coord = self.generate_neighbors(self._startX, self._startY)  # generate neighbors of start node
 		for i in neighbors_coord:
 			self.actions_to_execute.add((i, UNCOVER))  # add neighbors to set since everything around our start node is not a bomb
-
+		# self.actions_to_execute.add(((3, 4), UNCOVER))
+		# self.actions_to_execute.add(((2, 5), UNCOVER))
 		# #print("#printing initial model followed by the board")
 		#print_model(self._model)
 		# #print_board(self._board)
@@ -63,9 +64,11 @@ class MyAI( AI ):
 		self._uncovered_frontier = set()   # tiles that have been uncovered but not solved..
 
 	def getAction(self, number: int) -> "Action Object":
+
 		print("\nBEFORE ACTION")
-		print(self._uncovered_frontier)
-		print(self.actions_to_execute)
+		print("Uncovered Frontier", self._uncovered_frontier)
+		print("Covered Marked Frontier", self._covered_unmarked_frontier)
+		print("Known Actions", self.actions_to_execute)
 		print_model(self._model)
 		# print("MOVE COUNT:", self._moveCount)
 		# print("SIZE OF QUEUE", self.action_queue.qsize())
@@ -78,35 +81,48 @@ class MyAI( AI ):
 			current_tile.effective_label = number
 			self._update_model(x, y, current_tile.label)
 
-			print("NEW TILE UNLOCKED: ", x, y, number)
-			print_model(self._model)
+			# print("NEW TILE UNLOCKED: ", x, y, number)
+			# print_model(self._model)
 			self._uncover = (False, (-1, -1))
 			
-			if current_tile.effective_label == 0:  # if the number is 0, add the neighbors into the queue since there isn't a bomb around it
-				neighbors = self.generate_neighbors(x, y)
-				###print("NEIGHBORS", neighbors)
-				for i in neighbors:
-					###print(i)
-					if self._model[i[1]][i[0]].label == "*":
-						###print("add")
-						self.actions_to_execute.add((i, UNCOVER))
-			elif current_tile.effective_label > 0:  # greater than 0 -> add to uncovered_frontier
+			# check how model changed the neighbors
+			neighbors = self.generate_neighbors(x, y)
+			for i in neighbors:
+				if current_tile.effective_label == 0 and self._model[i[1]][i[0]].label == "*" :  # if the number is 0, add the neighbors into the queue since there isn't a bomb around it
+					print("\tadding uncover at ({}, {})".format(i[0],i[1]))
+					self.actions_to_execute.add((i, UNCOVER))
+					self.remove_covered_unmarked_neighbors(i)
+					continue
+				neighbor_tile = self._model[i[1]][i[0]]
+				if neighbor_tile.effective_label and neighbor_tile.effective_label == neighbor_tile.unvisited_neighbors:
+					#print(self._covered_unmarked_frontier)
+					for cu in self._covered_unmarked_frontier.copy():
+						if is_neighbor(cu, i):
+							print("\tadding flag at ({}, {})".format(cu[0], cu[1]))
+							self.actions_to_execute.add((cu, FLAG))
+							self.remove_covered_unmarked_neighbors(cu)
+
+			if current_tile.effective_label > 0:  # greater than 0 -> add to uncovered_frontier
 				self._uncovered_frontier.add((x, y))
-			
+				self.append_covered_unmarked_neighbors((x,y))
+			print("Uncovered Frontier", self._uncovered_frontier)
+			print("Covered Marked Frontier", self._covered_unmarked_frontier)
+			print("Known Actions", self.actions_to_execute)
 		if len(self._uncovered_frontier) != 0:
 			for coord in list(self._uncovered_frontier):
 				#print("coord to process", coord)
 				x, y = coord
 				tile = self._model[y][x]
-				if tile.effective_label == 0 and tile.unvisited_neighbors:
+				if tile.effective_label == 0 and tile.unvisited_neighbors: # uncover all neighbors
 					# should use covered_unmarked frontier instead of generating neighbors lol
 					###########################################
 					neighbors = self.generate_neighbors(x,y)
 					for n in neighbors:
 						if self._model[n[1]][n[0]].label == "*": 
-							self.actions_to_execute.add(((n[0], n[1]), UNCOVER))
+							self.actions_to_execute.add((n, UNCOVER))
+							self.remove_covered_unmarked_neighbors(n)
 					###########################################
-				elif tile.effective_label == tile.unvisited_neighbors:
+				elif tile.effective_label == tile.unvisited_neighbors:  # flag all neighbors
 					#print("FOUND BOMBS")
 					self._uncovered_frontier.remove(coord)
 					neighbors = self.generate_neighbors(x, y)
@@ -114,8 +130,18 @@ class MyAI( AI ):
 						##print(n)
 						if self._model[n[1]][n[0]].label == "*":  # found bomb(s) so add flag action
 							self.actions_to_execute.add(((n[0], n[1]), FLAG))
+							self.remove_covered_unmarked_neighbors(n)
+				elif len(self.actions_to_execute) == 0:
+				#	print("PATTERN")
+					self.check_pattern(coord)
+					# check patters
+					# check model
+		if len(self.actions_to_execute) == 0:
+			#backtrack
+			print("BACKTRACK")
+			self.backtracking_search()
 
-		if (len(self.actions_to_execute) != 0):   #if our action queue is not empty, uncover
+		if (len(self.actions_to_execute) != 0):   #if our action queue is not empty, do actions
 
 			coord, action = self.actions_to_execute.pop()
 			x, y = coord
@@ -124,7 +150,7 @@ class MyAI( AI ):
 				self._moveCount += 1
 				self._uncovered_tiles += 1
 				
-				# print("UNCOVER AT: ", x, y)
+				print("UNCOVER AT: ", x, y)
 				return Action(AI.Action(UNCOVER), x, y)
 			elif action == FLAG:
 				self._moveCount += 1
@@ -135,76 +161,8 @@ class MyAI( AI ):
 				self._update_model(x, y, "M")
 
 				# print_model(self._model)
-				# print("FLAG ACTION: ", x, y)
+				print("FLAG ACTION: ", x, y)
 				return Action(AI.Action(FLAG), x, y)
-
-		
-		#print("in else")
-		# if self._covered_unmarked_frontier.empty():  #!DESIGN: this would be better to generate this frontier or set as we perform actions, should also implement the other thing in slides
-		# 	unmarked_neighbors = set()
-		# 	for y, row in enumerate(self._model):  
-		# 		for x, tile in enumerate(row):
-		# 			if type(tile.label) is int and tile.unvisited_neighbors > 0:
-		# 				neighbors = self.generate_neighbors(x, y)
-		# 				found = 0
-		# 				for n in neighbors:
-		# 					if n in unmarked_neighbors:
-		# 						continue
-		# 					if found == tile.unvisited_neighbors:
-		# 						break
-		# 					if self._model[n[1]][n[0]].label == "*":
-		# 						unmarked_neighbors.add(n)
-		# 	for u in unmarked_neighbors:
-		# 		self._covered_unmarked_frontier.put(u)
-			
-		# if len(self._uncovered_frontier) == 0: #!DESIGN: could be better as a set for easy removal if make this in place
-		# 	for y, row in enumerate(self._model):  
-		# 		for x, tile in enumerate(row):
-		# 			if type(tile.label) is int and tile.unvisited_neighbors > 0:
-		# 				self._uncovered_frontier.add((x,y))				
-
-		# while len(self._uncovered_frontier) != 0:
-		# 	##print("IN WHILE")
-		# 	cur_X, cur_Y = self._uncovered_frontier.pop()
-		# 	print("POP", cur_X, cur_Y)
-		# 	tile = self._model[cur_Y][cur_X]
-
-		# 	if tile.effective_label == tile.unvisited_neighbors:
-		# 		neighbors = self.generate_neighbors(cur_X,cur_Y)
-		# 		for n in neighbors:
-		# 			##print(n)
-		# 			if self._model[n[1]][n[0]].label == "*":  # found bomb(s) so add flag action
-		# 				self.actions_to_execute.add(((n[0], n[1]), FLAG))
-
-		# 				self._moveCount += 1
-		# 		break # break out of while loop and execute these actions
-
-
-
-
-
-			# for y, row in enumerate(self._board):
-			# 	found_bomb = True
-			# 	if 1 in row:
-			# 		for x, num in enumerate(row):
-			# 			###print("COORD", x, y)
-			# 			if num == 1:
-			# 				###print("COORD", x, y)
-			# 				neighbors = self.generate_neighbors(x, y)
-			# 				potential_bombs = list()
-			# 				for n in neighbors:
-			# 					if len(potential_bombs) == 2:
-			# 						found_bomb = False
-			# 						break
-			# 					if self._board[n[1]][n[0]] == -1:
-			# 						potential_bombs.append(n)
-			# 				if found_bomb and len(potential_bombs) == 1:
-			# 					x1,y1 = potential_bombs[0]
-			# 					self._board[y1][x1] = "F"
-			# 					for n in self.generate_neighbors(x1, y1):
-			# 						self.action_queue.put(n)
-			# 					self._moveCount += 1
-			# 					return Action(AI.Action(FLAG), x1, y1)
 							
 		if self._uncovered_tiles == self._safe_spaces:  # we won the game
 			print("Completed Board...")			
@@ -295,18 +253,144 @@ class MyAI( AI ):
 					self._uncovered_frontier.remove((x,y))
 	
 	def backtracking_search(self):
+		# 1. order variables in V (covered_unmarked_frontier)
+		# -----> ordering by num of unvisitied neighbors
+		ordered = list()
+		for coord in self._covered_unmarked_frontier:
+			# get num of neighbors that are in uncovered frontier
+			num = len([i for i in self._uncovered_frontier if is_neighbor(i, coord)])
+			ordered.append((num, coord))
+
+		ordered_variables = sorted(ordered,key= lambda x:x[0], reverse=True)
+		print("Ordered Variables\n", ordered_variables)
+		constraints = {i: self._model[i[1]][i[0]].copy() for i in self._uncovered_frontier}
+		variables = {i: self._model[i[1]][i[0]].copy() for i in self._covered_unmarked_frontier}
+
+		print("constaints\n", constraints)
+		print("variables\n", variables)
+		
+		full_complete_assignments = list()
+		for covered in ordered_variables:
+			variables[covered].label = "M"
+			is_valid, updated_dict = self._check_update_constraints(covered, variables)
+			if is_valid:
+				variables = updated_dict
+			else:
+				variables[covered].label = "No Mine"
+			# check if neighbors veer it to bomb or no bomb, if not we assume it's a bomb
+
+	def _check_update_constraints(self, changed_coord, constraint_dict:dict) -> tuple:
+		"""
+		Based on a potential assignment, will check if the constraints are satisfied.
+		Returns a tuple that contains (boolean, updated onstraint_dict)
+		"""
+		changed_label = constraint_dict[changed_coord].label
+		if changed_label == "M":
+			for coord in constraint_dict.keys():
+			#check constraints
+				if self.is_neighbor(coord, changed_coord):
+					t = constraint_dict[coord]
+					if t.unvisited_neigbhors == 0 or t.effective_label == 0:
+						return (False, dict())
+					
+					# passes constrains so update
+					t.effective_label -= 1
+					t.unvisited_neigbhors -= 1
+
+		return (True, constraint_dict)
+
+	def _update_neighbors(self, changed_coord, constraint_dict):
+		"""
+		Based on an assignment, this will update neighbors
+		"""
 		pass
 
 
-	# def check_pattern(self, tile_coord: set) -> list:
-	# 	"""
-	# 	Checks the board for minesweeper patterns and returns actions based on that.
-	# 	"""
-	# 	return list()
+	def check_pattern(self, tile_coord: set):
+		"""
+		Checks the board for minesweeper patterns and adds actions based on that.
+		"""
+		x, y = tile_coord
+		tile = self._model[y][x]
+		
+		# get search space?
+		uncovered_neighbors = set()
+		covered_neighbors = set()
+
+		for coord in self._uncovered_frontier:
+			if is_neighbor((x,y), coord):
+				uncovered_neighbors.add(coord)
+		covered_neighbors = self._get_covered_unmarked_neighbors(tile_coord)
+		
+		# print(tile_coord)
+		# print("UN", uncovered_neighbors)
+		# print("CN", covered_neighbors)
+		# 1-1-x
+		if tile.unvisited_neighbors == 2:
+			for coordUN in uncovered_neighbors:
+				un_x, un_y = coordUN  # un = uncovered neighbor
+				if (un_x == x and abs(un_y - y) == 1) or (abs(un_x - x) == 1 and un_y == y): # if neighbor is right next to us
+					neighbor_tile = self._model[un_y][un_x]
+					if neighbor_tile.effective_label == 1 and neighbor_tile.unvisited_neighbors > 2:
+						print("1-1-x pattern: TRUE")
+						covered_neighbors2 = self._get_covered_unmarked_neighbors(coordUN)
+						print("CN2", covered_neighbors2)
+						for r in covered_neighbors2.difference(covered_neighbors):
+							print("Adding uncover...")
+							####################################
+							#shouldn't need if covered_unmarked is correct
+							potential_uncover_tile = self._model[r[1]][r[0]]
+							if potential_uncover_tile.label == "*":
+								self.actions_to_execute.add(((r), UNCOVER))
+								self.remove_covered_unmarked_neighbors(r)
+							print(r)
+						# now get the neighbors of this one from covered unmarked frontier and whichever ones they don't have in common can't be bombs
+
+					 # if neighbor is right next to us and has unvisited > 3
+
+	
+	def _get_covered_unmarked_neighbors(self, coord) -> set:
+	#	print("r",self._covered_unmarked_frontier)
+		x, y = coord	
+		return {c for c in self._covered_unmarked_frontier if is_neighbor((x,y), c)}	
+
+
+	def append_covered_unmarked_neighbors(self, coord):
+		"""
+		Appends coord's covered unmarked neighbors
+		"""
+		c_neighbors = self.generate_neighbors(coord[0], coord[1])
+		for n in c_neighbors:
+			if (n, 0) in self.actions_to_execute or (n, 1) in self.actions_to_execute:
+				# this means the tile is marked
+				continue
+			n_tile = self._model[n[1]][n[0]]
+			if n_tile.label == "*":
+				# this means the tile is covered & unmarked
+				self._covered_unmarked_frontier.add(n)
+	
+	def remove_covered_unmarked_neighbors(self, coord):
+		"""
+		Upon an action, will remove the coord since it would now be considered marked
+		"""
+		if coord in self._covered_unmarked_frontier:
+			self._covered_unmarked_frontier.remove(coord)
+def is_neighbor(a: set, b: set) -> bool:
+	"""
+	Assuming the coords are valid, returns true if they are neighbors.
+	"""
+	aX, aY = a
+	bX, bY = b
+	return False if a == b else abs(aX - bX) in (0, 1) and abs(aY - bY) in (0, 1)
 
 def print_model(model):
 	print("-----------------")
-	for i in model:
+	print(" ",end='  ')
+	for x in range(len(model)):
+		print("{:<13}".format(x), end='')
+	print()
+	for y, i in enumerate(model):
+		print(y, end= '  ')
 		for tile in i:
 			temp = "{:<13}"
 			print(temp.format(str(tile)), end = '')
@@ -328,6 +412,8 @@ def print_board(board):
 
 # run with  python Main.py -f .\Problems\Easy_world_1.txt for one world
 # run with  python Main.py -f .\Problems\ for all worlds
+# run with  python Main.py -f .\ProblemsBeginner\  # 362
+# run with  python Main.py -f .\ProblemsIntermediate\ # 65
 # python Main.py -f .\ProblemsBeginner\Beginner_world_1.txt
 
 # in openlab
@@ -342,3 +428,6 @@ class Tile():
 	
 	def __repr__(self):
 		return  "[" + str(self.label) + ", " + str(self.effective_label) + ", " + str(self.unvisited_neighbors) + "]"
+
+	def copy(self) -> 'Tile':
+		return Tile(self.label, self.effective_label, self.unvisited_neighbors)
